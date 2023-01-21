12882ba6e83884cbbda477dc5f08c06fe1e8e006
kkurant <115278738+kkurant@users.noreply.github.com>
2023-01-21 16:38:21 +0100

    Add files via upload

---
+++ JIPP_Bookstore/JIPP-Bookstore/Logic/BookManager.cs
@@ -0,0 +1,56 @@
+﻿using JIPP_Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JIPP_Bookstore.Logic
+{
+    public class BookManager
+    {
+        public void AddBook(BookModel bookModel)
+        {
+
+            using (var context = new BookContext())
+            {
+                context.Books.Add(bookModel);
+                context.SaveChanges();
+            }
+        }
+
+        public void RemoveBook(int id)
+        {
+            using(var context = new BookContext())
+            {
+                var bookToDelete = context.Books.Single(x => x.ID == id);
+                context.Books.Remove(bookToDelete);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateBook(BookModel bookModel)
+        {
+            using (var contex = new BookContext())
+            {
+                contex.Books.Update(bookModel);
+                contex.SaveChanges();
+            }
+        }
+
+        public BookModel GetBook(int id)
+        {
+            using (var context = new BookContext())
+            {
+                var book = context.Books.Single(x => x.ID == id);
+                return book;
+            }
+        }
+
+        public List<BookModel> GetBooks()
+        {
+            using (var context = new BookContext())
+            {
+                return context.Books.ToList();
+            }
+        }
+    }
+}
---
+++ JIPP_Bookstore/JIPP-Bookstore/Migrations/20230121003636_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JIPP_Bookstore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
