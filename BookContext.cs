00e49645e79ddab2919dee4d168c2cc92f85fcf1
kkurant <115278738+kkurant@users.noreply.github.com>
2023-01-21 16:20:21 +0100

    Add files via upload

---
+++ BookContext.cs
@@ -0,0 +1,16 @@
+﻿using JIPP_Bookstore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JIPP_Bookstore
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<BookModel> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //var connectionString = @"Data Source=DESKTOP-RVLTM1C\SQLEXPRESS02;Initial Catalog=BookstoreDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connectionString = @"Data Source=DESKTOP-RVLTM1C\SQLEXPRESS02;Initial Catalog=Bookstore_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
