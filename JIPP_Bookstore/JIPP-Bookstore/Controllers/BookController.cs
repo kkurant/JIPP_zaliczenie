7ac0c723f245205fb86d83891beceff9651a4f0d
kkurant <115278738+kkurant@users.noreply.github.com>
2023-01-21 16:38:33 +0100

    Add files via upload

---
+++ JIPP_Bookstore/JIPP-Bookstore/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using JIPP_Bookstore.Logic;
+using JIPP_Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JIPP_Bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        BookManager bookManager = new BookManager();
+        public IActionResult Index()
+        {
+            var books = bookManager.GetBooks();
+            
+            BookModel bookModel = new BookModel();
+            return View(books);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(BookModel book)
+        {
+            bookManager.AddBook(book);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            var book = bookManager.GetBook(id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveConfirm(int id)
+        {
+            bookManager.RemoveBook(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var book = bookManager.GetBook(id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BookModel book)
+        {
+            bookManager.UpdateBook(book);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var book = bookManager.GetBook(id);
+            return View(book);
+        }
+    }
+}
