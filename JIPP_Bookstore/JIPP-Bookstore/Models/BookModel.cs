6dd59f7cec27e9b1fe54a544e6bfdc60e979ed04
kkurant <115278738+kkurant@users.noreply.github.com>
2023-01-21 16:37:37 +0100

    Add files via upload

---
+++ JIPP_Bookstore/JIPP-Bookstore/Models/BookModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JIPP_Bookstore.Models
+
+{
+    public class BookModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Stock { get; set; }
+    }
+}
